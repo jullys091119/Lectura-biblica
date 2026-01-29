7934b124f91f1ec3c2be61efa31c51325b63dfa6
Julian <jullys091119@gmail.com>
2026-01-29 12:47:09 -0700

    cache start page for offline

--- next.config.js
+++ next.config.js
@@ -2,11 +2,20 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
   turbopack: {},
+  pwa: {
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) => url.pathname === "/",
+        handler: "CacheFirst",
+        options: {
+          cacheName: "start-page",
+        },
+      },
+    ],
+  },
 });
-console.log("NEXT CONFIG LOADED - PWA");
